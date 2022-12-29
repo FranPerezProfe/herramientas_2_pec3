3cea2ce43fc7d3f4329e3d1f448714ccef31134e
Fran Pérez <profesor.franciscojpa@ies-azarquiel.es>
2022-12-29 12:45:31 +0100

    Cambios en el header

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -31,28 +31,4 @@ document.addEventListener('DOMContentLoaded', function() {
           });
       }
   }
-
-  // close
-  const close = document.querySelectorAll('.navbar-close');
-  const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-  if (close.length) {
-      for (var i = 0; i < close.length; i++) {
-          close[i].addEventListener('click', function() {
-              for (var j = 0; j < menu.length; j++) {
-                  menu[j].classList.toggle('hidden');
-              }
-          });
-      }
-  }
-
-  if (backdrop.length) {
-      for (var i = 0; i < backdrop.length; i++) {
-          backdrop[i].addEventListener('click', function() {
-              for (var j = 0; j < menu.length; j++) {
-                  menu[j].classList.toggle('hidden');
-              }
-          });
-      }
-  }
 });
